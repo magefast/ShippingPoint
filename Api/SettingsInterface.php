935a52396771f480432e2d4df8dd87012d026a35
magefast <magefast@gmail.com>
2025-07-21 21:14:08 +0300

    update methods list, version composer

--- Api/SettingsInterface.php
+++ Api/SettingsInterface.php
@@ -6,6 +6,6 @@ interface SettingsInterface
 {
     public const ENABLED_CARRIER_METHOD = [
         'novaposhtabranch_novaposhtabranch' => 'novaposhtabranch_novaposhtabranch',
-        'novaposhtabranch_novaposhtaposhtomat' => 'novaposhtabranch_novaposhtaposhtomat',
+        'novaposhtaposhtomat_novaposhtaposhtomat' => 'novaposhtaposhtomat_novaposhtaposhtomat',
     ];
 }
